caddd7f8d669264c5ba5f79ae1a89d7ce709b6f6
Prof Edir <ecaramba@gmail.com>
2026-03-27 20:33:46 -0300

    exercios

---
+++ exe1.js
@@ -0,0 +1,14 @@
+
+function exe3()
+{
+    var n1 = prompt("Digite o num1");
+    var n2 = prompt("Digite o num2");
+
+    var soma = parseInt(n1) + parseInt(n2);
+    alert("a soma é: " + soma);
+}
+
+function exe5()
+{
+    
+}
